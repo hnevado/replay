4519ae9c284bacb327f527128cce6663d5721edc
Hector Nevado <hectornevado46@gmail.com>
2025-07-15 19:47:11 +0200

    Refactorización clase Validator

--- framework/Validator.php
+++ framework/Validator.php
@@ -20,22 +20,43 @@ class Validator {
             foreach ($rules as $rule) {
              
                 [$name, $param] = array_pad(explode(':', $rule), 2, null);
-
-                $error = match ($name) {
-                    'required' => empty($value) ? "$field es requerido." : null,
-                    'min' => strlen($value) < $param ? "$field debe tener al menos $param caracteres." : null,
-                    'max' => strlen($value) > $param ? "$field no puede exceder los $param caracteres." : null,
-                    'url' => filter_var($value, FILTER_VALIDATE_URL) === false ? "$field debe ser una URL válida." : null,
-                    default => null
-                };
                 
-                if ($error) {
+                if ($error = $this->hasError($name, $param, $field, $value)) {
                     $this->errors[] = $error;
                     break; // Stop checking further rules for this field if one fails
                 }
 
             }
         }
+    }
+
+    protected function hasError(string $name, ?string $param, string $field, string $value): ?string {
+
+         return match ($name) {
+                    'required' => $this->validateRequired($field,$value),
+                    'min' => $this->validateMin($field,$value, $param),
+                    'max' => $this->validateMax($field,$value, $param),
+                    'url' => $this->validateUrl($field,$value),
+                    default => null
+                };
+    }
+      
+    protected function validateRequired(string $field, mixed $value): ?string {
+        return empty($value) ? "$field es requerido." : null;
+    }
+
+    protected function validateMin(string $field, mixed $value, ?string $param): ?string {
+        $minLength = (int) $param;
+        return strlen($value) < $minLength ? "$field debe tener al menos $minLength caracteres." : null;
+    }
+
+    protected function validateMax(string $field, mixed $value, ?string $param): ?string {
+        $maxLength = (int) $param;
+        return strlen($value) > $maxLength ? "$field no puede exceder los $maxLength caracteres." : null;
+    }
+    
+    protected function validateUrl(string $field, mixed $value): ?string {
+        return filter_var($value, FILTER_VALIDATE_URL) === false ? "$field debe ser una URL válida." : null;
     }
 
     public function passes(): bool {
