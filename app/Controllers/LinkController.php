fe5d458eb2cfa57a4e4771c5076b4ddb6242218a
Hector Nevado <hectornevado46@gmail.com>
2025-07-17 18:54:55 +0200

    Creada constante para las reglas de validación

--- app/Controllers/LinkController.php
+++ app/Controllers/LinkController.php
@@ -1,6 +1,13 @@
 <?php 
 
+
 class LinkController {
+    
+    protected const LINK_RULES = [
+        'title'       => 'required|min:3|max:255',
+        'url'         => 'required|url|max:255',
+        'description' => 'required|min:15|max:500'
+    ];
 
     public function index() {
         $db = new Database();
@@ -49,11 +56,7 @@ class LinkController {
             exit('Link not found');
         }
 
-        $validator = new Validator($_POST, [
-            'title'         => 'required|min:3|max:255',
-            'url'           => 'required|url|max:255',
-            'description'   => 'required|min:15|max:500'
-        ]);
+        $validator = new Validator($_POST, self::LINK_RULES);
 
         if ($validator->passes()) {
             $db->query(
@@ -72,14 +75,10 @@ class LinkController {
         
         require_once __DIR__ . '/../../resources/edit-links.template.php';
     }   
-    
+
     public function store() { 
      
-        $validator = new Validator($_POST, [
-        'title'         => 'required|min:3|max:255',
-        'url'           => 'required|url|max:255',
-        'description'   => 'required|min:15|max:500'
-        ]);
+        $validator = new Validator($_POST, self::LINK_RULES);
         
 
         if ($validator->passes()) {
