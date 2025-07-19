7e6697ee7d3dedcef02a19b0b1c1da8b9eaf1f80
Hector Nevado <hectornevado46@gmail.com>
2025-07-19 19:17:27 +0200

    Creado fichero .env e instalación de la librería phpdotenv

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,5 @@
 <?php
-
+use Dotenv\Dotenv;
 require __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
--- config/app.php
+++ config/app.php
@@ -1,18 +1,18 @@
 <?php
 
 return [
-    'host' => 'localhost',
-    'dbname' => 'web-php',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8mb4',
+    'host' => $_ENV['DB_HOST'] ?? 'localhost',
+    'dbname' => $_ENV['DB_NAME'] ?? 'my_database',
+    'username' => $_ENV['DB_USER'] ?? 'root',
+    'password' => $_ENV['DB_PASS'] ?? '',
+    'charset' => $_ENV['DB_CHARSET'] ?? 'utf8mb4',
     'options' => [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ],
-    'app_name' => 'REPLAY',
-    'app_description' => 'Un sitio web dedicado a los videojuegos retro.',
-    'app_version' => '1.0.0',
-    'app_author' => 'Hector Nevado',
-    'app_url' => 'http://localhost/replay',
-    'csrf_token' => true,
+    'app_name' => $_ENV['APP_NAME'] ?? 'REPLAY',
+    'app_description' => $_ENV['APP_DESCRIPTION'] ?? '',
+    'app_version' => $_ENV['APP_VERSION'] ?? '',
+    'app_author' => $_ENV['APP_AUTHOR'] ?? '',
+    'app_url' => $_ENV['APP_URL'] ?? '',
+    'csrf_token' => $_ENV['CSRF_TOKEN'] ?? true,
 ];
